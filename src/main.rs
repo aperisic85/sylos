56923fe3049ba7ae7056425ce647c810dea4a0ec
Ante Perisic <perisic.ante@gmail.com>
2025-02-24 11:25:33 +0000

    add senders ip to parsed message

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use tokio::net::UdpSocket;
 
 #[tokio::main]
 async fn main() {
-    let socket: UdpSocket = UdpSocket::bind("0.0.0.0:1515").await.unwrap();
+    let socket: UdpSocket = UdpSocket::bind("0.0.0.0:1514").await.unwrap();
     println!("Listening on {}", socket.local_addr().unwrap());
 
     syslog::handler::handle_syslog_messages(&socket).await.unwrap();
--- src/syslog/handler.rs
+++ src/syslog/handler.rs
@@ -21,22 +21,24 @@ pub async fn handle_syslog_messages(socket: &UdpSocket) -> tokio::io::Result<()>
         if let Ok((_rest, parsed_log)) = parser::parse_syslog(message) {
             //println!("Parsed Log: {:?}", parsed_log);
             //filter by severity
-            let critical_severities = vec!["Emergency", "Alert", "Critical", "Error"];
-            if critical_severities.contains(&parsed_log.severity_name.as_str()) {
+           // let critical_severities = vec!["Emergency", "Alert", "Critical", "Error", "Notice"];
+           // if critical_severities.contains(&parsed_log.severity_name.as_str()) {
                 let log_entry = format!(
-                    "{} - {} - {} - {}\n",
+                    "{} - {} - {} - {} - {} - {}\n",
                     parsed_log.timestamp.unwrap_or_default(),
                     parsed_log.severity_name,
                     parsed_log.hostname,
-                    parsed_log.message
+                    parsed_log.message,
+                    src.ip(),
+                    src.port()
                 );
 
                 // Write the log entry to the file asynchronously
                 file.write_all(log_entry.as_bytes()).await?;
                 println!("Log saved to file!");
             }
-        } else {
-            println!("Failed to parse syslog message.");
-        }
+       // } else {
+          //  println!("Failed to parse syslog message.");
+     //   }
     }
 }
